5d49600cb5716a0544ecb37e616bdaa3ee91a77b
Ryan Victory <rvictory@gmail.com>
2013-04-09 08:07:24 -0500

    Simple chat program working in the demo

--- tests/test_socket_wrench.rb
+++ tests/test_socket_wrench.rb
@@ -10,11 +10,38 @@ EventMachine.run do
       set :server, :thin
       set :port, 9000
 
+      @@user_list = []
+
       @@socket = SocketWrench.new(9001)
-      @@socket.get_protocol("test").command("update") do |protocol, data, client, channel|
+
+      @@socket.get_protocol("chat").command("update") do |protocol, data, client, channel|
         # Send updates to all clients in the channel
         protocol.push(channel, "update", data)
       end
+
+      @@socket.get_protocol("chat").command("join") do |protocol, data, client, channel|
+        unless @@user_list.include? data['username']
+          # Tell the client the user list
+          protocol.push(client, "user_list", {'list' => @@user_list})
+          @@user_list.push(data['username'])
+          # Tell the clients that somebody joined
+          protocol.push(channel, "join", data)
+
+        end
+      end
+
+      @@socket.get_protocol("chat").command("leave") do |protocol, data, client, channel|
+        if @@user_list.include? data['username']
+          # Tell the clients that somebody joined
+          protocol.push(channel, "leave", data)
+          @@user_list.delete(data['username'])
+        end
+      end
+
+      @@socket.get_protocol("chat").command("username_check") do |protocol, data, client, channel|
+        protocol.push(client, "username_check", @@user_list.include?(data['username']))
+      end
+
       @@socket.begin_listening
 
     end
